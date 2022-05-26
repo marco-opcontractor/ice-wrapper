aa2eb15657728e120fbcd651f7c26637aa37dcd0
kgraveley <kgraveley@connexinsoftware.com>
2022-05-26 10:56:24 -0400

    Allowing cvx to be changed, so we can set defaults for other vaccines that come in with a blank cvx
    Release 1.0.1

--- src/main/kotlin/com/connexin/ice/wrapper/model/OPModels.kt
+++ src/main/kotlin/com/connexin/ice/wrapper/model/OPModels.kt
@@ -3,7 +3,7 @@ package com.connexin.ice.wrapper.model
 import java.time.LocalDate
 
 
-data class Vaccine(val id:String,val cvx:String,val name:String,val date:LocalDate)
+data class Vaccine(val id:String,var cvx:String,val name:String,val date:LocalDate)
 data class Indicator(val id:String?=null,val name:String?=null,val code:String,val system: CodeSystem,val interpretation: Interpretation,val date:LocalDate)
 data class VaccineReport(val gender:Gender,
                          val dateOfBirth: LocalDate,
