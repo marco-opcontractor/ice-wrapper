a605d85823510a31c25949d2d8e72c59c84761eb
Elmer Hurtado <ehurtado@opcontractor.com>
2024-09-11 17:31:50 -0400

    Move all rules inside ice wrapper instead of using them on hot ice

---
+++ src/main/kotlin/com/connexin/ice/wrapper/Constants.kt
@@ -0,0 +1,12 @@
+package com.connexin.ice.wrapper
+
+class Constants {
+    object FlagConstants {
+        const val FLAG_RSV_INDICATED = "rsvIndicated"
+        const val FLAG_SYNAGIS_INDICATED = "synagisIndicated"
+    }
+
+    object DiseaseCodes {
+        const val ICE_RSV_DISEASE_CODE = "079.6"
+    }
+}
